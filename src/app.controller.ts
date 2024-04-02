de6590dd5d0021c9326c61c0e75f4de5f472eb12
Ivan G Stewart <71293769+ivanstewart2001@users.noreply.github.com>
2024-04-02 02:46:03 -0400

    e

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -27,6 +27,11 @@ export class AppController {
 
   constructor(private readonly appService: AppService) {}
 
+  @Get()
+  getHello(): string {
+    return 'hello';
+  }
+
   @Get('/fetchBusinessProfile/:businessId/:userId')
   async fetchBusinessProfile(
     @Req() req: Request,
